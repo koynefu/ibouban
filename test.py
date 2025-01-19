bbb020182df54813fdb31488bfd4e316c7b279ba
AymenDaSh <dgoldmanb@gmail.com>
2025-01-18 19:47:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 print("an chedek an darbak")
+=======
+print("an chedek an dsdfdsiofjseiof jadf;olkjasn arbak")
+feskolfjnseoikfjseiofsj
+feisjfiosefjsef
+fesoijfsoiefjse
+>>>>>>> parent of 038926d (Update test.py)
